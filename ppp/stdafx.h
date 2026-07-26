b10404408b304178cc0eb85a07581e9096818ff0
Ling <lingyao0924@gmail.com>
2026-07-26 17:07:31 +0800

    chore(release): prepare v2.1.2

--- ppp/stdafx.h
+++ ppp/stdafx.h
@@ -259,7 +259,7 @@
 #endif
 
 #ifndef PPP_APPLICATION_VERSION
-#define PPP_APPLICATION_VERSION ("2.0.0.0") /* 2.0.0.0 */
+#define PPP_APPLICATION_VERSION ("2.1.2.0") /* 2.1.2.0 */
 #endif
 
 #ifndef PPP_APPLICATION_NAME
